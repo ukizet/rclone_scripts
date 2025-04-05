418bee8787e8d5557631ab2bc5b5920a0c783dcd
ukizet <ukikatuki@gmail.com>
2025-04-05 05:29:48 +0300

    moved from local ignore file to ignore file from vault. Added pull command

--- rclient.sh
+++ rclient.sh
@@ -11,14 +11,33 @@ bisync() {
   local local_folder="$1"
   local remote_folder="$2"
   local args="${3:-}"
-  exclude="--exclude-from $current_dir/.ignore"
+  exclude="--exclude-from $VAULT.rcloneignore"
 
   if [[ -n $args ]]; then
     echo -e "rclone bisync $local_folder $remote_folder --create-empty-src-dirs --verbose $args $exclude \n"
+    sleep 4
     rclone bisync "$local_folder" "$remote_folder" --create-empty-src-dirs --verbose "$args" $exclude || exit 1
   else
     echo -e "rclone bisync $local_folder $remote_folder --create-empty-src-dirs --verbose $exclude \n"
+    sleep 4
     rclone bisync "$local_folder" "$remote_folder" --create-empty-src-dirs --verbose $exclude || exit 1
+  fi
+}
+
+copy() {
+  local source="$1"
+  local dest="$2"
+  local args="${3:-}"
+  exclude="--exclude-from $VAULT.rcloneignore"
+
+  if [[ -n $args ]]; then
+    echo -e "rclone copy $source $dest --create-empty-src-dirs --verbose $args $exclude \n"
+    sleep 4
+    rclone copy "$source" "$dest" --create-empty-src-dirs --verbose "$args" $exclude || exit 1
+  else
+    echo -e "rclone copy $source $dest --create-empty-src-dirs --verbose $exclude \n"
+    sleep 4
+    rclone copy "$source" "$dest" --create-empty-src-dirs --verbose $exclude || exit 1
   fi
 }
 
@@ -36,10 +55,11 @@ while [[ $# -gt 0 ]]; do
   --help)
     echo "
 bisync     - bisync 'rclone bisync local_folder remote_folder --create-empty-src-dirs --verbose exclude || exit 1'
-testbisync - testbisync
+testbisync - testbisync 'bisync with --dry-run'
 resync     - first bisync
-testresync - testresync
+testresync - testresync 'resync with --dry-run'
 init       - initialize rclone
+pull       - copy files from remote
 --help     - this massage
 "
     shift
@@ -62,6 +82,14 @@ init       - initialize rclone
     ;;
   init)
     create_config
+    shift
+    ;;
+  pull)
+    copy "$VAULTREMOTE" "$VAULT"
+    shift
+    ;;
+  testpull)
+    copy "$VAULTREMOTE" "$VAULT" "--dry-run"
     shift
     ;;
   *)
